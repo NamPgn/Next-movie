67b48bd5173f5b18d4b94b06cad9e0d8bf2c9039
nampgn <nampham18082003@gmail.com>
2024-08-21 21:15:31 +0700

    update

--- src/app/components/Teamplates/Header/index.tsx
+++ src/app/components/Teamplates/Header/index.tsx
@@ -44,7 +44,7 @@ export default function Header() {
                 placeholder="Tìm kiếm phim..."
                 className="bg-[#333] text-white px-4 py-2 rounded-full w-64 focus:outline-none focus:ring-2 focus:ring-yellow-400"
               />
-              <button className="absolute right-3 top-1/2 transform -translate-y-1/2">
+              <button className="absolute right-3 top-1/2 transform -translate-y-1/2" title="Tìm kiếm">
                 <svg
                   className="w-5 h-5 text-gray-400"
                   fill="none"
@@ -76,7 +76,7 @@ export default function Header() {
             </div>
 
             {/* Mobile Menu Button */}
-            <button className="md:hidden">
+            <button className="md:hidden" title="menu">
               <svg
                 className="w-6 h-6"
                 fill="none"
