00496562a80509ccbc8cdbd78a93608363293d1c
nampg <nampham18082003@gmail.com>
2024-11-27 23:36:14 +0700

    update

--- next.config.js
+++ next.config.js
@@ -11,23 +11,27 @@ module.exports = async (phase, { defaultConfig }) => {
       ],
     },
     webpack(config) {
+      // Lấy rule xử lý file SVG mặc định của Next.js
       const fileLoaderRule = config.module.rules.find((rule) =>
         rule.test?.test?.('.svg')
       );
 
+      // Cập nhật rule để hỗ trợ cả `@svgr/webpack` và xử lý SVG dạng URL
       config.module.rules.push(
         {
           ...fileLoaderRule,
           test: /\.svg$/i,
-          resourceQuery: /url/,
+          resourceQuery: /url/, // *.svg?url
         },
         {
           test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          resourceQuery: { not: [/url/] }, 
-          use: ["@svgr/webpack"],
+          issuer: /\.[jt]sx?$/, // Xử lý khi được import trong JS/TS
+          resourceQuery: { not: [/url/] }, // Không áp dụng nếu có `?url`
+          use: ["@svgr/webpack"], // Sử dụng @svgr/webpack
         }
       );
+
+      // Loại trừ SVG khỏi rule mặc định của Next.js
       fileLoaderRule.exclude = /\.svg$/i;
 
       return config;
