e8d80940fa0de6ae59623f6bdda3d5e8ab617683
NamPgn <101863356+NamPgn@users.noreply.github.com>
2024-12-07 14:30:44 +0700

    Update categorySevices.ts

--- src/app/sevices/categorySevices.ts
+++ src/app/sevices/categorySevices.ts
@@ -12,7 +12,7 @@ export async function fetchCategorysRecentllyUpdated(): Promise<FetchCategoriesR
         `${process.env.NEXT_PUBLIC_API_BASE_URL}/category/latest/next`,
         {
           method: "GET",
-          next: { revalidate: 10 },
+          cache: "no-cache",
         }
       )
     ).json();
