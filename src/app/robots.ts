82775a1f435b393a88e12efe6abbc92c9e7609b7
nampg <nampham18082003@gmail.com>
2025-03-09 17:50:58 +0700

    update ads.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://hoathinhtrungquoc.site/sitemap.xml',
+    sitemap: 'https://hhhihi.site/sitemap.xml',
   }
 }
